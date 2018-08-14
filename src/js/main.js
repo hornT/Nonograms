7b6ba78f34e2b3c0124ceae61e7419a3304d7572
Неверов Николай <neverov@sicon.ru>
2018-08-14 14:51:57 +0300

    add jq

---
+++ src/js/main.js
@@ -0,0 +1,65 @@
+'use strict';
+
+$(document).ready(function () {
+    var dropZone = $('#dropZone');
+
+
+    if (typeof (window.FileReader) === 'undefined') {
+        dropZone.text('Не поддерживается браузером!');
+        dropZone.addClass('error');
+    }
+
+    dropZone[0].ondragover = function () {
+        dropZone.addClass('hover');
+        return false;
+    };
+
+    dropZone[0].ondragleave = function () {
+        dropZone.removeClass('hover');
+        return false;
+    };
+
+    dropZone[0].ondrop = function (event) {
+        event.preventDefault();
+        dropZone.removeClass('hover');
+        dropZone.addClass('drop');
+
+        const file = event.dataTransfer.files[0];
+        const reader = new FileReader();
+        //reader.onload = onFileLoad;
+        reader.onload = function (e) {
+            onFileLoad(e, file.name);
+        };
+        reader.readAsDataURL(file);
+    };
+});
+
+function log(text) {
+    console.log(text);
+}
+
+function dropZoneOnClick() {
+    $('#fileInput').trigger('click');
+}
+
+function fileChange(e) {
+
+    var reader = new FileReader();
+    var files = e.dataTransfer ? e.dataTransfer.files : e.target.files;
+
+    reader.onload = function (e) {
+        onFileLoad(e, files[0].name);
+    };
+    reader.readAsDataURL(files[0]);
+}
+
+/**
+ * Загрузка файла
+ * @param {any} e
+ * @param {any} fileName
+ */
+function onFileLoad(e, fileName) {
+    const data = e.target.result;
+
+    log(1);
+}
